2b51dfdf2d8732d3e8501c86960b4cabf1aa17fd
svnny-aka-sarah <116078227+svnny-aka-sarah@users.noreply.github.com>
2022-12-01 21:30:24 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas =createCanvas(550, 550);
+    canvas.position(550, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
